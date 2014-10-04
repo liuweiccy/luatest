35a6831acedf83734c81aed02fd4f6b216d5f62e
EricLiu <liuweiccy@users.noreply.github.com>
2014-10-04 23:10:25 +0800

    lua测试

---
+++ luatest1/test1.lua
@@ -0,0 +1,9 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Administrator
+-- Date: 14-10-4
+-- Time: 下午10:58
+-- To change this template use File | Settings | File Templates.
+--
+print("hello world")
+
