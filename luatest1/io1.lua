8c1e4712c5aaac8f75133fab8336a133193a7035
EricLiu <liuweiccy@users.noreply.github.com>
2014-10-19 20:01:20 +0800

    io测试

--- luatest1/io1.lua
+++ luatest1/io1.lua
@@ -5,12 +5,15 @@
 -- Time: 8:42
 -- To change this template use File | Settings | File Templates.
 --
-line = io.read()
-n = tonumber(line)
-if n == nil then
-    error(line .. "is not a vaild number")
-else
-    print(2*n)
+--line = io.read()
+--n = tonumber(line)
+--if n == nil then
+--    error(line .. "is not a vaild number")
+--else
+--    print(2*n)
+--end
+
+
+for line in io.lines() do
+   io.write(line,'\n')
 end
-
-
